bcc9ff1f4d2e7eb412dc34c1cc04fa8ecb776d4d
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-29 14:53:45 +0545

    navbar done

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import NavLink from "./navLink";
+
 const navLinks = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
-  { url: "/portfolio", title: "Portfolio" },
-  { url: "/contact", title: "Contact" },
+  { url: "/quotes", title: "KrishnasQuotes" },
+  { url: "/bhgavadai", title: "BhagvadAI" },
 ];
-const navbar = () => {
+const NavBar = () => {
   return (
-    <div>
-      
+    <div className="flex justify-between my-4 p-4 bg-gradient-to-r from-slate-500 via-black ">
+      <div className="hidden md:flex gap-4 w-1/3 text-white">
+        {navLinks.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
+      <div>
+        logo
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+export default NavBar;
