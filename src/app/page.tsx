6a4393c476917dbc19ae63938bc5c5c4fde54cc0
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-29 14:11:50 +0545

    creating pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ListIcons from "@/components/atoms/list";
 import Link from "next/link";
 
 interface ChapterInfo {
@@ -29,20 +30,26 @@ const Home = async () => {
   const data = await fetchData();
 
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-2 gap-4 my-10">
       {data?.map((rep, index) => (
-        <div key={index} className="flex flex-col mt-10">
-          <div className="border p-4 rounded">
-            <h1 className="font-bold flex gap-2 text-xl text-purple-700">
-              Chapter
-              <span className="text-blue-700">{rep.chapter_number}</span>
-            </h1>
-            <h1 className="text-orange-400 font-medium">{rep.translation}</h1>
-            <p className="line-clamp-3 ">{rep.summary.en}</p>
-            <Link href="/">
-              <p>{rep.verses_count} Verses</p>
-            </Link>
-          </div>
+        <div
+          key={index}
+          className="flex p-4 border rounded-2xl flex-col hover:shadow-lg  shadow-indigo-500/40  bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100 "
+        >
+          <h1 className="font-bold flex gap-2  text-xl text-purple-700">
+            Chapter
+            <span className="text-blue-700">{rep.chapter_number}</span>
+          </h1>
+          <h1 className="text-orange-600 font-medium pb-2">
+            {rep.translation}
+          </h1>
+          <p className="line-clamp-3 mb-2 text-justify ">{rep.summary.en}</p>
+          <Link href="/" className="w-fit">
+            <p className="flex gap-2 font-bold">
+              <ListIcons /> {rep.verses_count}
+              <span className="text-purple-700 font-semibold">Verses</span>
+            </p>
+          </Link>
         </div>
       ))}
     </div>
