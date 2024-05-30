2c65714dfbec3a2490b2db14cdb3b18c15aeef14
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-30 15:04:27 +0545

    adding chatbot in app

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,32 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { NextResponse } from "next/server";
+
+
+export const runtime = "edge";
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
+
+export async function POST(req: Request) {
+  try {
+    if (!process.env.OPENAI_API_KEY) {
+      return new NextResponse("Missing OpenAI API Key.", { status: 400 });
+    }
+
+    const { messages } = await req.json();
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages,
+    });
+
+    // Deduct credits
+   
+
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error: any) {
+    return new NextResponse(error.message || "Something went wrong!", {
+      status: 500,
+    });
+  }
+}
--- src/app/bhgavadai/page.tsx
+++ src/app/bhgavadai/page.tsx
@@ -1,9 +1,11 @@
+import Chat from '@/components/chat';
 import React from 'react'
 
 const BhagvadAI = () => {
   return (
     <div>
       aiii
+      <Chat/>
     </div>
   )
 }
