4a006df8016299d5142c0249334235f3b0a00a55
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-05-30 07:24:34 +0545

    data fetching sucessfull realted to chapter

--- src/app/chapter/[id]/page.tsx
+++ src/app/chapter/[id]/page.tsx
@@ -1,16 +1,45 @@
-"use client"
-import { usePathname, useSearchParams } from 'next/navigation'
-import React from 'react'
+"use client";
 
+import { usePathname, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+interface ChapterInfo {
+  chapter_number: number;
+  verses_count: number;
+  name: string;
+  translation: string;
+  transliteration: string;
+  meaning: {
+    en: string;
+    hi: string;
+  };
+  summary: {
+    en: string;
+    hi: string;
+  };
+}
 const page = () => {
-    const searchParmas=usePathname()
-    console.log("senene",searchParmas)
+  const [data, setData] = useState<ChapterInfo>();
+  const searchParmas = usePathname();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(`https://bhagavadgitaapi.in/${searchParmas}`);
+        const result: ChapterInfo = await resp.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      heeleleoeoeoeo
-      <p>{searchParmas}</p>
+      <h1>hello</h1>
+      {data?.summary.en}
     </div>
   );
-}
+};
 
-export default page
+export default page;
--- src/app/chapter/page.tsx
+++ src/app/chapter/page.tsx
@@ -1,50 +1,50 @@
-"use client";
-import { usePathname, useSearchParams } from "next/navigation";
-import React from "react";
+// "use client";
+// import { usePathname, useSearchParams } from "next/navigation";
+// import React from "react";
 
-interface ChapterInfo {
-  chapter_number: number;
-  verses_count: number;
-  name: string;
-  translation: string;
-  transliteration: string;
-  meaning: {
-    en: string;
-    hi: string;
-  };
-  summary: {
-    en: string;
-    hi: string;
-  };
-}
-const ChapterDescription = async () => {
-  const pathname = usePathname();
-  console.log(pathname)
-  const fetchData = async () => {
-    try {
-      const resp = await fetch(`https://bhagavadgitaapi.in/${pathname}`);
-      const result: ChapterInfo[] = await resp.json();
-      return result;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-  const data = await fetchData();
-  return (
-    <div>
-      {/* {data?.map((chapter,index)=>(
-            <div key={index}>
-                <p>{chapter.summary.en}</p>
+// interface ChapterInfo {
+//   chapter_number: number;
+//   verses_count: number;
+//   name: string;
+//   translation: string;
+//   transliteration: string;
+//   meaning: {
+//     en: string;
+//     hi: string;
+//   };
+//   summary: {
+//     en: string;
+//     hi: string;
+//   };
+// }
+// const ChapterDescription = async () => {
+//   const pathname = usePathname();
+//   console.log(pathname)
+//   const fetchData = async () => {
+//     try {
+//       const resp = await fetch(`https://bhagavadgitaapi.in/${pathname}`);
+//       const result: ChapterInfo[] = await resp.json();
+//       return result;
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
+//   const data = await fetchData();
+//   return (
+//     <div>
+//       {/* {data?.map((chapter,index)=>(
+//             <div key={index}>
+//                 <p>{chapter.summary.en}</p>
 
-            </div>
-        ))} */}
-      {data?.map((chap, index) => (
-        <div key={index}>
-          <p>{chap.summary.en}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+//             </div>
+//         ))} */}
+//       {data?.map((chap, index) => (
+//         <div key={index}>
+//           <p>{chap.summary.en}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default ChapterDescription;
+// export default ChapterDescription;
