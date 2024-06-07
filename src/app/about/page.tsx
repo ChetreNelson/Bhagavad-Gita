6369c779f942629997b566b2320b663dbb6bb49f
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-06-07 10:03:30 +0545

    making heros card

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,47 +1,44 @@
-
+import Image from "next/image";
+import AboutBackground from "../../../public/About/AboutCanvas.jpeg";
 const About = () => {
   return (
-    <div>
+    <>
       <section>
-
-      <p>background inisde which the below pargraph will be contained</p>
-      <p>
-        The Bhagavad Gita is a sacred text from the Indian epic Mahabharata,
-        known for its deep spiritual and philosophical insights. It tells the
-        story of a conversation between Prince Arjuna and his charioteer, Lord
-        Krishna, on the battlefield of Kurukshetra.
-        <span>
-          Facing a moral crisis about fighting in the war, Arjuna turns to
-          Krishna for guidance. Krishna shares profound teachings on duty
-          (dharma), action (karma), and the different paths to spiritual growth
-          (yoga). He explains the nature of the soul, the importance of living a
-          righteous life, and the power of devotion (bhakti).
-        </span>
-        The Gita offers timeless wisdom on how to navigate life's challenges
-        with clarity and purpose. It inspires people to live with integrity,
-        perform their duties selflessly, and seek a deeper connection with the
-        divine. Even today, the Bhagavad Gita continues to guide and uplift
-        those seeking spiritual understanding and inner peace.
-      </p>
-
+        <div className="w-full h-[420px]  relative ">
+          <Image
+            className="rounded-2xl"
+            src={AboutBackground}
+            alt="canvas"
+            layout="fill"
+            objectFit="cover"
+          />
+          <p className="absolute select-none font-bold m-20 text-justify text-white text-xl">
+            The Bhagavad Gita is a sacred text from the Indian epic Mahabharata,
+            known for its deep spiritual and philosophical insights. It tells
+            the story of a conversation between Prince Arjuna and his
+            charioteer, Lord Krishna, on the battlefield of Kurukshetra. Facing
+            a moral crisis about fighting in the war, Arjuna turns to Krishna
+            for guidance.<br/> Krishna shares profound teachings on duty (dharma),
+            action (karma), and the different paths to spiritual growth (yoga).
+            He explains the nature of the soul, the importance of living a
+            righteous life, and the power of devotion (bhakti). The Gita offers
+            timeless wisdom on how to navigate life's challenges with clarity
+            and purpose. It inspires people to live with integrity, perform
+            their duties selflessly, and seek a deeper connection with the
+            divine. Even today, the Bhagavad Gita continues to guide and uplift
+            those seeking spiritual understanding and inner peace.
+          </p>
+        </div>
       </section>
 
-
-      <section className="mt-10">
-        Mythical events  cards
-      </section>
+      <section className="mt-10">sadhgurus and other personal saying</section>
+      <section className="mt-10">Mythical events cards</section>
       <section className="mt-10">
         mythical arts and drwaings related to Mahabharata
       </section>
 
-
-      <section className="mt-10">
-        sadhgurus and other personal saying
-      </section>
-      <section>
-
-      </section>
-    </div>
+      <section></section>
+    </>
   );
 };
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,11 +9,12 @@ const navLinks = [
   { url: "/about", title: "About" },
   { url: "/quotes", title: "KrishnasQuotes" },
   { url: "/bhgavadai", title: "BhagvadAI" },
+  { url: "/testings", title: "Testings" }
 ];
 const NavBar = () => {
   return (
     <>
-      <div className="flex  sticky  top-0 bg-white justify-between my-4 p-4 bg-gradient-to-r from-slate-500 via-black ">
+      <div className="flex z-10  sticky  top-0 bg-white justify-between my-4 p-4 bg-gradient-to-r from-slate-500 via-black ">
         <div className="hidden  md:flex gap-4 w-1/3 text-white">
           {navLinks.map((link) => (
             <NavLink link={link} key={link.title} />
@@ -21,12 +22,12 @@ const NavBar = () => {
         </div>
       </div>
       <Link href='/'>
-        <Image
+        {/* <Image
           className="rounded absolute top-0 right-[120px]"
           src={Logo}
           width={200}
           alt="Bhagavad Gita"
-        />
+        /> */}
       </Link>
     </>
   );
