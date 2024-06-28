77c6566ad9481c4ae7945a2dc71f486cbfa5a8fa
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-06-28 13:42:32 +0545

    issue resolved

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import AboutBackground from "../../../public/About/AboutCanvas.jpeg";
+
 const About = () => {
   return (
     <>
       <section>
-        <div className="w-full h-[420px]  relative ">
+        <div className="w-full h-[420px] relative">
           <Image
             className="rounded-2xl"
             src={AboutBackground}
@@ -18,15 +19,16 @@ const About = () => {
             the story of a conversation between Prince Arjuna and his
             charioteer, Lord Krishna, on the battlefield of Kurukshetra. Facing
             a moral crisis about fighting in the war, Arjuna turns to Krishna
-            for guidance.<br/> Krishna shares profound teachings on duty (dharma),
-            action (karma), and the different paths to spiritual growth (yoga).
-            He explains the nature of the soul, the importance of living a
+            for guidance.
+            <br /> Krishna shares profound teachings on duty (dharma), action
+            (karma), and the different paths to spiritual growth (yoga). He
+            explains the nature of the soul, the importance of living a
             righteous life, and the power of devotion (bhakti). The Gita offers
-            timeless wisdom on how to navigate life's challenges with clarity
-            and purpose. It inspires people to live with integrity, perform
-            their duties selflessly, and seek a deeper connection with the
-            divine. Even today, the Bhagavad Gita continues to guide and uplift
-            those seeking spiritual understanding and inner peace.
+            timeless wisdom on how to navigate life&apos;s challenges with
+            clarity and purpose. It inspires people to live with integrity,
+            perform their duties selflessly, and seek a deeper connection with
+            the divine. Even today, the Bhagavad Gita continues to guide and
+            uplift those seeking spiritual understanding and inner peace.
           </p>
         </div>
       </section>
@@ -34,10 +36,8 @@ const About = () => {
       <section className="mt-10">sadhgurus and other personal saying</section>
       <section className="mt-10">Mythical events cards</section>
       <section className="mt-10">
-        mythical arts and drwaings related to Mahabharata
+        mythical arts and drawings related to Mahabharata
       </section>
-
-      <section></section>
     </>
   );
 };
